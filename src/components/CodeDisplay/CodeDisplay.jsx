3d1e7665962d78abd72afc11cde9902aa1e55912
Kevin Janise <kevinjanise@gmail.com>
2025-04-10 09:45:10 -0500

    Added checkmark when copying code

--- src/components/CodeDisplay/CodeDisplay.jsx
+++ src/components/CodeDisplay/CodeDisplay.jsx
@@ -1,4 +1,6 @@
 import styles from "./CodeDisplay.module.css";
+
+import { useState } from "react";
 import iconCopy from "./icon_copy.svg";
 import iconZip from "./icon_zip.svg";
 import * as Utils from "utils/Utils";
@@ -10,15 +12,27 @@ function CodeDisplay({
   style = {},
   ...rest
 }) {
+  const [wasCopied, setWasCopied] = useState(false);
+
   const handleCopyToClipboard = () => {
     Utils.copyToClipboard(sourceCode);
+    setWasCopied(true);
   };
 
   const combinedClassName = `${styles.codeDisplay} ${className}`;
 
   return (
     <div className={combinedClassName} style={style} {...rest}>
-      <h3 className={styles.title}>{title} <img src={iconCopy} className={styles.copyIcon} alt="Copy code" onClick={handleCopyToClipboard}/></h3>
+      <h3 className={styles.title}>
+        {wasCopied && <span className={styles.checkMark}>&#10003;</span>}
+        {title}{" "}
+        <img
+          src={iconCopy}
+          className={styles.copyIcon}
+          alt="Copy code"
+          onClick={handleCopyToClipboard}
+        />
+      </h3>
 
       <pre>
         <code className={styles.code}>{sourceCode}</code>
